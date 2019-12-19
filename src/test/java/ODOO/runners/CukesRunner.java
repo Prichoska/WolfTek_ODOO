26d063760aff4be3c10aed268c9703af77b93060
Prichoska <Prichoska@yahoo.com>
2019-12-19 16:51:08 -0500

    first commit

---
+++ src/test/java/ODOO/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package ODOO.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json"},
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false,
+        tags = "@wip"
+)
+
+public class CukesRunner {
+}
